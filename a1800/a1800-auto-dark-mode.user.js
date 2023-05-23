f66341e78418c13cb71250b639448a6ea8bf1872
adjagu <6255623+adjagu@users.noreply.github.com>
2023-05-22 21:42:04 -0400

    Update a1800-auto-dark-mode.user.js
    Remove `@match` for a1800.net, bump version.

--- a1800/a1800-auto-dark-mode.user.js
+++ a1800/a1800-auto-dark-mode.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
-// @name        Auto Dark Mode : a1800.net
+// @name        Auto Dark Mode : a1800
 // @namespace   userscript@adjagu
-// @match       https://a1800.net/
 // @match       https://a1800.s1.zetohosting.pl/
 // @grant       none
 // @license     GPL-2.0-only
 // @author      adjagu
 // @description Apply "dark-mode" CSS class to body element.
-// @version     0.0.2
+// @version     0.0.3
 // @icon        https://raw.githubusercontent.com/adjagu/userscripts/main/a1800/icon/a1800.png
 // @downloadURL https://raw.githubusercontent.com/adjagu/userscripts/main/a1800/a1800-auto-dark-mode.user.js
 // @homepageURL https://github.com/adjagu/userscripts
